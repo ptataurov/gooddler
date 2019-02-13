54aa69e2d1348aea0c048e6fecba2fc692c0c345
Pavel Tataurov <p.tataurov92@gmail.com>
2019-02-13 16:35:39 +0300

    update

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -3,18 +3,15 @@ import './form.scss';
 const btns = document.querySelectorAll('.button');
 const form = document.querySelector('.form');
 const btnClose = document.querySelector('.form__close');
-const body = document.body;
 
 const btnsContact = Array.from(btns).filter(btn => btn.textContent === 'Contact Us' || btn.textContent === 'Contact us');
 
 btnsContact.forEach(btn => {
 	btn.onclick = () => {
-		// body.classList.add('fixed');
 		form.classList.add('form--show');
 	}
 })
 
 btnClose.onclick = () => {
-	// body.classList.remove('fixed');
 	form.classList.remove('form--show');
 };
